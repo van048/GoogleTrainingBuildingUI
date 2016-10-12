d0372c19eb3cbafdbcd7b732dc84fa9e260dc054
van048 <yybbenyang@gmail.com>
2016-10-12 16:45:56 +0800

    刪除多餘 import

--- app/src/main/java/cn/ben/googleplaytrainingbuildingui/MainActivity.java
+++ app/src/main/java/cn/ben/googleplaytrainingbuildingui/MainActivity.java
@@ -4,10 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
