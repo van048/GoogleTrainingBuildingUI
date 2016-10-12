6227a1c8b15e3ec72e74b7ccde79cbbfca1c5bb0
van048 <yybbenyang@gmail.com>
2016-10-12 16:44:05 +0800

    Use the String Resources

--- app/src/main/java/cn/ben/googleplaytrainingbuildingui/DisplayMessageActivity.java
+++ app/src/main/java/cn/ben/googleplaytrainingbuildingui/DisplayMessageActivity.java
@@ -1,10 +1,16 @@
 package cn.ben.googleplaytrainingbuildingui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.RelativeLayout.ALIGN_PARENT_BOTTOM;
+import static android.widget.RelativeLayout.BELOW;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -20,6 +26,28 @@ public class DisplayMessageActivity extends AppCompatActivity {
         textView.setText(message);
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(BELOW, R.id.btn_use_string_res_2);
+        textView.setLayoutParams(layoutParams);
+        layout.addView(textView);
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    public void useStringResource1(View view) {
+        // Get a string resource from your app's Resources
+        String hello = getResources().getString(R.string.hello_world);
+        Toast.makeText(this, hello, Toast.LENGTH_SHORT).show();
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    public void useStringResource2(View view) {
+        // Or supply a string resource to a method that requires a string
+        TextView textView = new TextView(this);
+        textView.setText(R.string.hello_world);
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(ALIGN_PARENT_BOTTOM);
+        textView.setLayoutParams(layoutParams);
         layout.addView(textView);
     }
 }
--- app/src/main/java/cn/ben/googleplaytrainingbuildingui/MainActivity.java
+++ app/src/main/java/cn/ben/googleplaytrainingbuildingui/MainActivity.java
@@ -4,17 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    /** Called when the user clicks the Send button */
+    /**
+     * Called when the user clicks the Send button
+     */
     @SuppressWarnings("UnusedParameters")
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
