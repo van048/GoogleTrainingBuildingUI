857872b3dfb836b674c541e42d43e43271b52cab
van048 <yybbenyang@gmail.com>
2016-10-12 15:57:20 +0800

    review

--- app/src/main/java/cn/ben/googleplaytrainingbuildingui/MainActivity.java
+++ app/src/main/java/cn/ben/googleplaytrainingbuildingui/MainActivity.java
@@ -11,48 +11,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "extra_message";
-    private static final String TAG = "BEN";
-
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Get a string resource from your app's Resources
-        String appName = getResources().getString(R.string.app_name);
-        Log.d(TAG, appName);
-        // Or supply a string resource to a method that requires a string
-        TextView textView = (TextView) findViewById(R.id.tv_app_name);
-        textView.setText(R.string.app_name);
     }
 
-    /**
-     * Called when the user clicks the Send button
-     */
-    @SuppressWarnings("unused")
-    public void sendMessage(@SuppressWarnings("UnusedParameters") View view) {
-        // Do something in response to button
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
-
-    /**
-     * Check System Version at Runtime
-     */
-    @SuppressWarnings("unused")
-    private void setUpActionBar() {
-        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            ActionBar actionBar = getActionBar();
-            if (actionBar != null) {
-                // TODO: 2016/8/31
-                actionBar.setDisplayHomeAsUpEnabled(true);
-            }
-        }
     }
 }
