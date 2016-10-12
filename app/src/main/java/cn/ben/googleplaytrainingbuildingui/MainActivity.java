033848ac61d0a267dfeb9d4a79c043e89950d98e
van048 <yybbenyang@gmail.com>
2016-10-12 16:49:53 +0800

    Check System Version at Runtime

--- app/src/main/java/cn/ben/googleplaytrainingbuildingui/MainActivity.java
+++ app/src/main/java/cn/ben/googleplaytrainingbuildingui/MainActivity.java
@@ -1,6 +1,8 @@
 package cn.ben.googleplaytrainingbuildingui;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpActionBar();
     }
 
     /**
@@ -25,5 +29,15 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    private void setUpActionBar() {
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+            }
+        }
     }
 }
